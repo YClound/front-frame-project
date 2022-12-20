815b97d82361d4bd756e509b6cc14560f3edd4c4
yanan.g055 <yanan.g055@shulidata.com>
2022-12-20 11:58:53 +0800

    feat(vite): vite

---
+++ vite/nested/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '@/App';
+import styles from './index.module.less';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className={styles['c-red']}>222222222</div>
+  </React.StrictMode>,
+)
---
+++ vite/vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+        nested: resolve(__dirname, './nested/index.html'),
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './nested'),
+    }
+  }
+})
+
